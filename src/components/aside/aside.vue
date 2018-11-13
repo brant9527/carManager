29600e75d48fa9d8efb306387fbcc9685e8a6456
brant9527 <398707650@qq.com>
2018-11-13 17:33:21 +0800

    布局

---
+++ src/components/aside/aside.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="">   
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
